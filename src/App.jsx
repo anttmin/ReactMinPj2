a7529b12e20404c395be0b087a324cf101143660
anttmin <anttminm@gamil.com>
2024-04-10 11:25:07 +0630

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,96 @@
 import React, { useState } from "react";
-
 const data = [
   {
     id: "1",
     question: "what are accordion components",
     answer:
-      "accordion components are used interface element used for original and presenting",
+      "accordion components are used interface element used for original and presentingaccordion components are used interface element used for original and presenting",
   },
   {
     id: "2",
     question: "what are they used for components",
     answer:
-      "accordion components are used interface element used for original and presenting",
+      "accordion components are used interface element used for original and presenting accordion components are used interface element used for original and presenting",
   },
   {
     id: "3",
     question: "Accordian as a musical instruments",
     answer:
-      "accordion components are used interface element used for original and presenting",
+      "accordion components are used interface element used for original and presenting accordion components are used interface element used for original and presenting",
   },
   {
     id: "4",
     question:
       "Can i create an accordion components with a different framework?",
     answer:
-      "accordion components are used interface element used for original and presenting",
+      "accordion components are used interface element used for original and presenting accordion components are used interface element used for original and presenting",
   },
 ];
 
 const App = () => {
-  const [selected, setSelected] = useState(null);
-  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
-  const [multiple, setMultiple] = useState([]);
+  const [selected, setselected] = useState(null);
+  const [EnableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setmultiple] = useState([]);
 
   function handleSingleSelection(getCurrentId) {
-    //  setSelected(getCurrentId) //to add id
-    setSelected(getCurrentId === selected ? null : getCurrentId); //to remove current id
-  }
-
-  function handleMultiSelection(getCurrentId) {
-    let cpyMutiple = [...multiple];
-    console.log(cpyMutiple);
-    const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId);
-    console.log(findIndexOfCurrentId);
-
-    if (findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId);
-
-    else cpyMutiple.splice(findIndexOfCurrentId, 1);
-    console.log(cpyMutiple)
-
-    setMultiple(cpyMutiple);
+    setselected(selected === getCurrentId ? null : getCurrentId);
   }
 
   // console.log(selected);
-  
+
+  function handleMultiSelection(getCurrentId) {
+    //ဘာလိုarrayနဲ့ထည့်ကလည်းဆိုတော့ arry အလွတ်ထဲကနေ indexOfနဲရှာမယ် နဂိုထဲကအလွတ်ဆိုတော့ -1ထွက်မယ်အဲဒီ-1နဲ့ချိတ်ကမယ်အဲဒါကkeyပဲ****
+    let cpyMutiple = [...multiple];
+    console.log(cpyMutiple);//အလွတ်
+    console.log(getCurrentId);//id တေွဝင်မယ်
+
+    const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId);//ပြီးရင်cpyထဲမှာရှာမယ်မတေွရင်-1ထုတ်
+    console.log(findIndexOfCurrentId);
+
+    if (findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId);//-1နဲ့ညီရင်cpy arrayထဲကိုid တေွကိုထည့်မယ်
+    else cpyMutiple.splice(findIndexOfCurrentId, 1);//မဟုတ်ရင်arryထဲကid1ခုချင်းဆီလေျာမယ်1ခုချင်းစီ
+
+    setmultiple(cpyMutiple);
+    
+
+   
+
+  }
+
   console.log(multiple);
+
   return (
-    <div className="Wrapper">
-      <div>
+    <div>
+      <div className="Accordian">
         <button
-          className="BtnClick"
-          onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+          className="Multi"
+          onClick={() => setEnableMultiSelection(!EnableMultiSelection)}
         >
           Enable Multi Selection
         </button>
-      </div>
-      <div className="accordian">
-        {data && data.length > 0 ? (
-          data.map((dataItem) => (
-            <div className="item" key={dataItem.id}>
+        {data && data.length > 0 ?
+           ( data.map((DatatItem) => (
+            <div className="Accordian1" key={DatatItem.id}>
               <div
-                className="title"
+                className="Title"
                 onClick={
-                  enableMultiSelection
-                    ? () => handleMultiSelection(dataItem.id)
-                    : () => handleSingleSelection(dataItem.id)
+                  EnableMultiSelection
+                    ? () => handleMultiSelection(DatatItem.id)
+                    : () => handleSingleSelection(DatatItem.id)
                 }
               >
-                <h3>{dataItem.question}</h3>
+                <h3>{DatatItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ||
-              multiple.indexOf(dataItem.id) !== -1 ? (
-                <div>{dataItem.answer}</div>
+
+              {selected === DatatItem.id ||
+              multiple.indexOf(DatatItem.id) !== -1 ? (
+                <p>{DatatItem.answer}</p>
               ) : null}
+
             </div>
-          ))
-        ) : (
-          <div>NO DATA</div>
-        )}
+          )))
+         : (<div>No data</div>) }
       </div>
     </div>
   );
